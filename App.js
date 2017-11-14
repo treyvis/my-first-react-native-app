9c0f8f1be9425d17a408abc4f86da216118f78e5
Trey <treyvis@gmail.com>
2017-11-14 09:36:47 -0700

    Playing around with props

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+class Hello extends React.Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}</Text>
+    );
+  }
+}
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Hello name='Trey' />
+        <Hello name='Aubrey' />
       </View>
     );
   }
@@ -16,8 +23,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#bbb',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
 });
