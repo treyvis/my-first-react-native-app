a74305205ac1be3b5c6ae33f0f55fe61f12c2261
Trey <treyvis@gmail.com>
2017-11-16 10:32:31 -0700

    Playing around with flex

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 class Hello extends React.Component {
   render() {
     return (
-      <Text>Hello {this.props.name}</Text>
+      <Text style={styles.headings}>Hello {this.props.name}</Text>
     );
   }
 }
@@ -13,11 +13,13 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Hello name='Trey' />
-        <Hello name='Aubrey' />
+        <View style={{flex: 1}}>
+          <Hello name='Trey' />
+          <Hello name='Aubrey' />
+        </View>
         <Image 
           source={{uri: 'https://s3-media4.fl.yelpcdn.com/bphoto/6lYNesrYPiU2Y4_HuFpysA/348s.jpg'}}
-          style={{width: '100%', height: 200}}
+          style={{width: '100%', flex: 1}}
         />
       </View>
     );
@@ -27,8 +29,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'powderblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headings: {
+    fontSize: 40,
+    color: 'white'
+  }
 });
